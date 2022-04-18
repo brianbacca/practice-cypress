83714820da5b490cde11522e178de4befe4814c2
brianbacca <brianbn05@gmail.com>
2022-04-18 15:03:18 -0300

    config Node and express

---
+++ backEnd-testing/src/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const pino = require('pino-http')();
+const logger = require('pino')();
+
+const app = express();
+app.use(pino);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  logger.info(`server is running on port ${port}`);
+});
